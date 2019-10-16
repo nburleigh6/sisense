29079994249285afacf9339f47d713d6d8ec62fe
nburleigh6 <nburleigh@gmail.com>
2019-10-16 10:02:55 -0700

    Create word_wrap_indicator.js

---
+++ js/word_wrap_indicator.js
@@ -0,0 +1,7 @@
+//////////////////////////////////////////////
+/*** WORD WRAP INDICATOR WIDGET TITLE ***/////
+//////////////////////////////////////////////
+
+widget.on('ready', function (se, ev) {
+	$('#title_span').html('Average Downloads<br>per Organization<br> &nbsp;');  // put the text for your indicator here
+});
